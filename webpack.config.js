a739119a6c7aabeb3a833b9e146778d3c6e0f496
みみつき <okajima02k@gmail.com>
2021-12-22 01:58:46 +0900

    webpack開発サーバ設定

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,47 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
   mode: 'development',
 
   entry: './src/main.ts',
-  output: {
-    path: `${__dirname}/dist`,
-    filename: 'main.js'
-  },
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader'
+        use: 'ts-loader',
+        exclude: /node_modules/
       },
       {
         test: /\.(gif|png|jpg|eot|wof|woff|ttf|svg)$/,
         type: 'asset/inline'
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
   resolve: {
     extensions: ['.ts', '.js']
   },
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'main.bundle.js',
+    clean: true
+  },
   devServer: {
-      static: {
-          directory: `${__dirname}/dist`
-      }
-  }
+    static: {
+      directory: `${__dirname}/dist`
+    },
+    hot: true,
+    watchFiles: ['src/**/*.ts', 'dist/**/*']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development'
+    })
+  ],
+  target: 'web'
 };
